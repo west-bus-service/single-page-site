b810373d81c738ed4ae09bb614b771b8988ae3c3
Max Katz-Christy <maxkatzchristy@gmail.com>
2025-07-24 07:35:18 -0400

    use stops.geojson as source of truth

--- scripts/gen_gtfs.py
+++ scripts/gen_gtfs.py
@@ -381,223 +381,26 @@ TRIPS = [
     },
 ]
 
-STOPS = [
-    # WCC
-    {
-        "stop_id": "STOP-9034671c-e991-4439-9e71-a05a8599a56f",
-        "stop_name": "Calais",
-        "stop_desc": "Marden's Parking Lot",
-        "stop_lat": 45.18854669392266,
-        "stop_lon": -67.28220840306957,
-    },
-    {
-        "stop_id": "STOP-14ed8aff-0f08-4ac4-89fe-e9b172db07bb",
-        "stop_name": "Perry",
-        "stop_desc": "Wabanaki Mall",
-        "stop_lat": 44.97231404736198,
-        "stop_lon": -67.07493005575536,
-    },
-    {
-        "stop_id": "STOP-2ad237c0-4dfe-4a50-a8cd-cec8972e1922",
-        "stop_name": "Pembroke",
-        "stop_desc": "Corner of Rt. 214 & Rt. 1",
-        "stop_lat": 44.94965682737453,
-        "stop_lon": -67.17849654043128,
-    },
-    {
-        "stop_id": "STOP-c48d97aa-f7b1-4560-b697-048d4aca8f74",
-        "stop_name": "Dennysville",
-        "stop_desc": "Cobscook Bay Cafe",
-        "stop_lat": 44.90308868913789,
-        "stop_lon": -67.22050781904481,
-    },
-    {
-        "stop_id": "STOP-d530cc40-12bd-4071-95de-52fbaf1d7d23",
-        "stop_name": "Whiting",
-        "stop_desc": "Community Center/Store",
-        "stop_lat": 44.791233559261826,
-        "stop_lon": -67.17424929425094,
-    },
-    {
-        "stop_id": "STOP-477b3ace-4389-47e8-a4c7-cb32cd874a10",
-        "stop_name": "Machias",
-        "stop_desc": "Irving Mainway",
-        "stop_lat": 44.71747416614576,
-        "stop_lon": -67.45320159645519,
-    },
-    {
-        "stop_id": "STOP-3186da65-c835-47ae-9095-f3881148ffc9",
-        "stop_name": "Jonesboro",
-        "stop_desc": "Swamp Yankee BBQ",
-        "stop_lat": 44.662060972756706,
-        "stop_lon": -67.57294144907351,
-    },
-    {
-        "stop_id": "STOP-be554e9d-f180-450f-902b-a772328ac4dd",
-        "stop_name": "Columbia",
-        "stop_desc": "Elmer's Discount",
-        "stop_lat": 44.64287175736857,
-        "stop_lon": -67.76299771361364,
-    },
-    {
-        "stop_id": "STOP-7a5a3410-4d73-4cbf-aea1-df3d4aa243f2",
-        "stop_name": "Milbridge",
-        "stop_desc": "44 North Rest.",
-        "stop_lat": 44.537812397946055,
-        "stop_lon": -67.88149880431949,
-    },
-    {
-        "stop_id": "STOP-e354cfda-7054-4ed6-b5c4-e7a4975cfdbd",
-        "stop_name": "Gouldsboro",
-        "stop_desc": "Young's Market",
-        "stop_lat": 44.484226237813715,
-        "stop_lon": -68.1149969959795,
-    },
-    {
-        "stop_id": "STOP-970a9e30-06d7-4ddb-98cc-49cdee5588cb",
-        "stop_name": "Sullivan",
-        "stop_desc": "Sullivan REC. Center",
-        "stop_lat": 44.53024290489276,
-        "stop_lon": -68.22476574771778,
-    },
-    {
-        "stop_id": "STOP-fc0ebfb0-a667-4382-88d3-d3f194bf26b6",
-        "stop_name": "Hancock",
-        "stop_desc": "Village Store",
-        "stop_lat": 44.53014123436881,
-        "stop_lon": -68.25858959486574,
-    },
-    {
-        "stop_id": "STOP-2cd4673a-e3d0-472f-9654-37a6c9233e4a",
-        "stop_name": "Ellsworth",
-        "stop_desc": "Main Entrance Mill Mall, Out Back",
-        "stop_lat": 44.551403972284845,
-        "stop_lon": -68.43000587506339,
-    },
-    {
-        "stop_id": "STOP-3012643a-132d-4c0d-974e-cdc8714f9368",
-        "stop_name": "Bangor (Concord Trailways)",
-        "stop_desc": "Concord Trailways",
-        "stop_lat": 44.816594110637595,
-        "stop_lon": -68.80876371853913,
-    },
-    {
-        "stop_id": "STOP-8f56e854-d898-49f8-aef1-f6420a0c6042",
-        "stop_name": "Bangor (Airport)",
-        "stop_desc": "Bangor Airport",
-        "stop_lat": 44.80905167866493,
-        "stop_lon": -68.81786055142301,
-    },
-    # ELLS
-    {
-        "stop_desc": "Post Office",
-        "stop_id": "STOP-b11bb36f-65f5-4bfe-abb8-bdf1d76ac3b9",
-        "stop_lat": 44.51930662452464,
-        "stop_lon": -67.61194499329405,
-        "stop_name": "Beals Island",
-    },
-    {
-        "stop_desc": "Post Office",
-        "stop_id": "STOP-cce489d3-8d6c-4e48-813d-c2d1d2acb90b",
-        "stop_lat": 44.531579513613664,
-        "stop_lon": -67.60136565981361,
-        "stop_name": "Jonesport",
-    },
-    {
-        "stop_desc": "Town Office",
-        "stop_id": "STOP-709e5965-7b27-4c47-a1de-1eedbea4350e",
-        "stop_lat": 44.65327843995525,
-        "stop_lon": -67.72815087072122,
-        "stop_name": "Addison",
-    },
-    {
-        "stop_desc": "Pleasant View Appartments",
-        "stop_id": "STOP-fc77e641-ea79-4e44-b050-16cf14331355",
-        "stop_lat": 44.6358184790437,
-        "stop_lon": -67.73386558458837,
-        "stop_name": "Columbia Falls",
-    },
-    {
-        "stop_desc": "4 Corners",
-        "stop_id": "STOP-a8fb5add-97f2-4284-8533-1f2181fdc6f8",
-        "stop_lat": 44.64318156877917,
-        "stop_lon": -67.76194701794302,
-        "stop_name": "Columbia",
-    },
-    {
-        "stop_desc": "Housing Appartments",
-        "stop_id": "STOP-a64ef87a-091d-47ea-9c17-8f1652e1d882",
-        "stop_lat": 44.6201213065024,
-        "stop_lon": -67.80826855268631,
-        "stop_name": "Harrington",
-    },
-    {
-        "stop_desc": "Narraguagus Estates",
-        "stop_id": "STOP-fc06a983-3770-4ca9-882e-4b90cd0c3573",
-        "stop_lat": 44.59677148932213,
-        "stop_lon": -67.93183540873198,
-        "stop_name": "Cherryfield",
-    },
-    {
-        "stop_desc": "West Manor",
-        "stop_id": "STOP-d6a9c3c6-72f3-47fb-9317-bd223cbd3764",
-        "stop_lat": 44.53754398311699,
-        "stop_lon": -67.87995630656948,
-        "stop_name": "Milbridge",
-    },
-    {
-        "stop_desc": "Town Office",
-        "stop_id": "STOP-e3f84be2-a7a6-43f9-8489-76cefb81d9c0",
-        "stop_lat": 44.513255761192,
-        "stop_lon": -67.96625333615914,
-        "stop_name": "Steuben",
-    },
-    {
-        "stop_desc": "Town",
-        "stop_id": "STOP-14ae1c74-7f7c-4204-be30-265e030a1c35",
-        "stop_lat": 44.54300199772945,
-        "stop_lon": -68.42019965314486,
-        "stop_name": "Ellsworth",
-    },
-    # Lubec
-    {
-        "stop_desc": "Town",
-        "stop_id": "STOP-421bb314-1894-4c15-bb6f-0fec17fcb7d9",
-        "stop_lat": 44.85869201099502,
-        "stop_lon": -66.98336351926885,
-        "stop_name": "Lubec",
-    },
-    # School Route
-    {
-        "stop_desc": "Franklin Trading Post",
-        "stop_id": "STOP-1332dd93-e6a0-45e1-b4fd-940d57659703",
-        "stop_lat": 44.58890243818803,
-        "stop_lon": -68.22318410664123,
-        "stop_name": "Franklin",
-    },
-    {
-        "stop_desc": "Winter Harbor Garage",
-        "stop_id": "STOP-1c531172-cfcf-4843-8de5-ca23289d30b5",
-        "stop_lat": 44.394562204134324,
-        "stop_lon": -68.0851666416681,
-        "stop_name": "Winter Harbor",
-    },
-]
+# Load stops from stops.geojson
+def load_stops():
+    with open(f"{script_dir}/stops.geojson", "r", encoding="utf-8") as f:
+        stops_geojson = geojson.load(f)
+    
+    stops = []
+    for feature in stops_geojson["features"]:
+        stop = {
+            "stop_id": feature["properties"]["stop_id"],
+            "stop_name": feature["properties"]["stop_name"],
+            "stop_desc": feature["properties"]["stop_desc"],
+            "stop_lat": feature["geometry"]["coordinates"][1],  # lat is second coordinate
+            "stop_lon": feature["geometry"]["coordinates"][0],  # lon is first coordinate
+        }
+        stops.append(stop)
+    return stops
 
-# Generate stops.geojson
-fc = geojson.FeatureCollection([
-    geojson.Feature(
-        geometry=geojson.Point((s["stop_lon"], s["stop_lat"])),
-        properties={
-            "stop_id": s["stop_id"],
-            "stop_name": s["stop_name"],
-            "stop_desc": s["stop_desc"],
-        },
-    )
-    for s in STOPS
-])
-with open(f"{script_dir}/stops.geojson", "w") as f:
-    geojson.dump(fc, f, sort_keys=True, indent=2)
+STOPS = load_stops()
+
+# stops.geojson is now the source of truth - no longer generated here
 
 # Generate route URLs
 stop_lookup = {s["stop_id"]: (s["stop_lon"], s["stop_lat"]) for s in STOPS}
